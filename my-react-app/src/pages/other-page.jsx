784150d86fb3b861ffc98f3211cb1d6fed52a447
Flavio <flavkupe@umich.edu>
2021-02-08 01:29:14 -0800

    A few more tweaks

--- my-react-app/src/pages/other-page.jsx
+++ my-react-app/src/pages/other-page.jsx
@@ -4,19 +4,25 @@ function OtherPage() {
 
     const [dataState, setDataState] = React.useState("loading...");
 
-    fetch("/api/getdata").catch((err) => {
-        console.error(err);
-    }).then(async (a) => {
-        const data = await a.json();
-        
-        console.log("data", data);
-        setDataState(data.theData);
-    });
+    React.useEffect(() => {
+        fetch("/api/getdata").catch((err) => {
+            console.error(err);
+        }).then(async (a) => {
+            const data = await a.json();
+            
+            console.log("data", data);
+            setDataState(data.theData);
+        });
+    }, []);
 
     return (
         <div>
             This is the other page.
-            <div>Here is some data: {dataState}</div>
+            <div>Here is some data: 
+                <div className={"data-text"}>
+                    {dataState}
+                </div>
+            </div>
         </div>
     )
 }
